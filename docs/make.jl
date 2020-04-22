792cf9968aed9b4bcfc5fe112f0db58a9e3c3ce5
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-04-22 12:51:21 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
-push!(LOAD_PATH,"src/solver/gen.jl")
-push!(LOAD_PATH,"/Users/AnR/Dropbox/basic-profile/basic-program-share/src/solver/modules")
-using Documenter, gen
+#push!(LOAD_PATH,"src/solver/gen.jl")
+#push!(LOAD_PATH,"/Users/AnR/Dropbox/basic-profile/basic-program-share/src/solver/modules")
+using Documenter, socialsolver
 
 makedocs(sitename="Strack Group Project Documentation")
